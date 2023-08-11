01e3e37b50c423a32144b918d5dd84e166eea9d5
SebastianLamota <lamota.as@gmail.com>
2023-08-11 05:36:09 -0300

    ramauno

--- src/ItemDetailContainer/ItemCount.js
+++ src/ItemDetailContainer/ItemCount.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const Count = () => {
-    const [base, variar] = useState(0);
+const Count = ({inicial, stock, onAdd}) => {
+    const [base, variar] = useState(inicial);
 
     const sumar = () => {
         variar(base + 1);
@@ -14,19 +13,19 @@ const Count = () => {
 
     //Ponemos un useEffect para que cada vez que agregue al carrito se ponga en cero base
     useEffect(() => {
-        variar(parseInt(base));
-    }, [base])
+        variar(parseInt(inicial));
+    }, [inicial])
 //No me funcionó.
 
 return (
         <div className='botoneraCount'>
             <div className='btnSumaResta'>
-                <button disabled={base <= 0} onClick={restar} className='btnResta'>Restar</button>
+                <button disabled={base <= 1} onClick={restar} className='btnResta'>Restar</button>
                 <h3 className='visualCount'> {base} </h3>
-                <button disabled={base >= 10} onClick={sumar} className='btnSuma'>Sumar</button>
+                <button disabled={base >= stock} onClick={sumar} className='btnSuma'>Sumar</button>
             </div>
             <div className='btnCart' >
-                <button className='btnAgrCart' >Agregar al carrito</button>
+                <button disabled={base <= 0} onClick={() => onAdd(base)} className='btnAgrCart'>Agregar al carrito</button>
             </div>
         </div>
     )
--- src/ItemDetailContainer/ItemDetail.js
+++ src/ItemDetailContainer/ItemDetail.js
@@ -3,16 +3,20 @@ import ItemCount from './ItemCount';
 
 const ItemDetail = ({item}) => {
 
+    const onAdd = (cantidad) => {
+        console.log(`Compraste ${cantidad} unidades`);
+    }
+
     return (
         <div>
             <div>
-                <img src={item.img} className="imgDetail" alt={item.description}/>
-                <h2>{item.description}</h2>
-                <p>{item.motor} Nm</p>
+                <img src={item.imagen2} className="imgDetail" alt={item.category}/>
+                <h2>{item.tipomotor}</h2>
+                <p>{item.motor} CV</p>
                 <p>$ {item.price}</p>
             </div>
             <div>
-                <ItemCount stockItem={item.stock} />
+                <ItemCount stock={item.stock} inicial={0} onAdd={onAdd}/>
             </div>
         </div>
     )
