681a32c432daf964caef1e8084d87b922b704726
SebastianLamota <lamota.as@gmail.com>
2023-08-16 03:10:46 -0300

    ramauno

--- src/Otros.js
+++ src/Otros.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Otros = () => {
 
-    
     return (
         <div className="bg-gy">
             <div className="pd-50px">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuiKjLQEHGEp2UGuVx8wQr6WWiOgPcKe0",
+  authDomain: "newbk-c95b6.firebaseapp.com",
+  projectId: "newbk-c95b6",
+  storageBucket: "newbk-c95b6.appspot.com",
+  messagingSenderId: "646915804604",
+  appId: "1:646915804604:web:b17beaf198e57e7a73afb9"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
